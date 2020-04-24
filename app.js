77303e69cdfa11ebe8264796e51cb0bbeff814fb
Vlad Blank <blankvladp@gmail.com>
2020-04-24 22:36:36 +0300

    add the sound of each pad

--- app.js
+++ app.js
@@ -11,14 +11,45 @@ class DrumKit {
     this.bpm = 150;
   }
 
+  // toggle 'active' class method
   activePad(){
     this.classList.toggle('active');
   }
 
   repeat(){
+    // create a step that will go from 0 to 7
     let step = this.index % 8;
+
+    // select each pad from the 3 trakcs:
+    // [div.pad.kick-pad.b0, div.pad.snare-pad.b0, div.pad.hihat-pad.b0]
+    // [div.pad.kick-pad.b1, div.pad.snare-pad.b1, div.pad.hihat-pad.b1]
+    // [div.pad.kick-pad.b2, div.pad.snare-pad.b2, div.pad.hihat-pad.b2]
+    // etc..
     const activePads = document.querySelectorAll(`.b${step}`);
     console.log(activePads);
+    
+    
+    activePads.forEach( (pad) => {
+      // animate each pad while looping through them
+      pad.style.animation = `playTrack .3s alternate ease-in-out 2`;
+      
+      // check if pads have class of active
+      if(pad.classList.contains('active')){
+        // check each sound
+        if(pad.classList.contains('kick-pad')){
+          this.kickAudio.currentTime = 0;
+          this.kickAudio.play();
+        } else if(pad.classList.contains('snare-pad')){
+          this.snareAudio.currentTime = 0;
+          this.snareAudio.play();
+        } else if(pad.classList.contains('hihat-pad')){
+          this.hihatAudio.currentTime = 0;
+          this.hihatAudio.play();
+        }
+      } 
+    });
+
+    // increment the index by 1
     this.index++;
   }
 
@@ -30,11 +61,17 @@ class DrumKit {
   }
 }
 
+
+// create a drumkit
 const drumKit = new DrumKit();
 
 
 drumKit.pads.forEach( (pad) => {
+  // add 'active' class to each clicked pad or removes it if it has it.
+  // in this case we don't need to call the activePad() method in a callback fn or arrow function becase the "this" keyword is pointing to each clicked pad
   pad.addEventListener('click', drumKit.activePad);
+  // remove the animation property after the animation ends.
+  pad.addEventListener('animationend', () => pad.style.animation = '');
 });
 
 
